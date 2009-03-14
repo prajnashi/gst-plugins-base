4bdf9ee01e7f9c16586a514b9105647de8174f0a
Edward Hervey <bilboed@bilboed.com>
2009-03-14 13:05:17 +0100

    Wrap audioconvert, cleanup top-level Android.mk

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ GST_PLUGINS_BASE_TOP := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
+# base libraries
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/tag/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/audio/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/video/Android.mk
@@ -11,9 +12,13 @@ include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/riff/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/pbutils/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/rtp/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst-libs/gst/rtsp/Android.mk
+
+# plugins without external dependencies
+include $(GST_PLUGINS_BASE_TOP)/gst/audiotestsrc/Android.mk
+include $(GST_PLUGINS_BASE_TOP)/gst/audioconvert/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst/ffmpegcolorspace/Android.mk
+include $(GST_PLUGINS_BASE_TOP)/gst/gdp/Android.mk
+include $(GST_PLUGINS_BASE_TOP)/gst/playback/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst/videoscale/Android.mk
 include $(GST_PLUGINS_BASE_TOP)/gst/videotestsrc/Android.mk
-include $(GST_PLUGINS_BASE_TOP)/gst/audiotestsrc/Android.mk
-include $(GST_PLUGINS_BASE_TOP)/gst/playback/Android.mk
-include $(GST_PLUGINS_BASE_TOP)/gst/gdp/Android.mk
+
