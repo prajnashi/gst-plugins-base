f3205da1963c3b68407421455e0717f10dcbb922
Edward Hervey <bilboed@bilboed.com>
2009-04-01 15:01:43 +0200

    Wrap decodebin2 also

--- gst/playback/Android.mk
+++ gst/playback/Android.mk
@@ -51,3 +51,50 @@ LOCAL_CFLAGS := \
 	-DHAVE_CONFIG_H	
 
 include $(BUILD_PLUGIN_LIBRARY)
+
+###### DECODEBIN2
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= 		\
+	gstdecodebin2.c		\
+	gsturidecodebin.c	\
+	gstfactorylists.c	\
+	gstplay-enum.c		\
+	android/gstplay-marshal.c
+
+LOCAL_SHARED_LIBRARIES := 	\
+	libgstreamer-0.10	\
+	libgstbase-0.10		\
+	libgstpbutils-0.10	\
+	libglib-2.0		\
+	libgthread-2.0		\
+	libgmodule-2.0		\
+	libgobject-2.0
+
+LOCAL_MODULE:= libgstdecodebin2
+
+LOCAL_C_INCLUDES := 			\
+	$(LOCAL_PATH)			\
+	$(LOCAL_PATH)/android		\
+	$(GST_PLUGINS_BASE_TOP)		\
+	$(GST_PLUGINS_BASE_TOP)/android	\
+	$(GST_PLUGINS_BASE_TOP)/gst-libs\
+	$(GST_PLUGINS_BASE_TOP)/gst-libs/gst/pbutils/android \
+	external/liboil			\
+	external/gstreamer		\
+	external/gstreamer/android 	\
+	external/gstreamer/libs		\
+	external/gstreamer/gst		\
+	external/gstreamer/gst/android	\
+	external/glib			\
+	external/glib/glib		\
+	external/glib/android	  	\
+	external/glib/gmodule	  	\
+	external/glib/gobject	  	\
+	external/glib/gthread
+
+LOCAL_CFLAGS := \
+	-DHAVE_CONFIG_H	
+
+include $(BUILD_PLUGIN_LIBRARY)
